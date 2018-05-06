a56255f70ac50e917a847e8f83883907708d234f
MadCat <orloffski@gmail.com>
2018-05-07 00:22:14 +0300

    Rev 0.6 check error

--- app/src/main/java/development/madcat/dagger2example/components/AppComponent.java
+++ app/src/main/java/development/madcat/dagger2example/components/AppComponent.java
@@ -3,10 +3,11 @@ package development.madcat.dagger2example.components;
 import dagger.Component;
 import development.madcat.dagger2example.classes.DatabaseHelper;
 import development.madcat.dagger2example.classes.NetworkUtils;
+import development.madcat.dagger2example.modules.NetworkModule;
 import development.madcat.dagger2example.modules.StorageModule;
 
-@Component(modules = {StorageModule.class, NetworkUtils.class})
+@Component(modules = {StorageModule.class, NetworkModule.class})
 public interface AppComponent {
-    DatabaseHelper getDatabaseHelper();
     NetworkUtils getNetworkUtils();
+    DatabaseHelper getDatabaseHelper();
 }
