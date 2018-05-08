62b574d1b08d20e17e590a0ca5b49d1be6a8cbec
MadCat <orloffski@gmail.com>
2018-05-08 23:48:07 +0300

    Rev 0.8 test Dagger2 injects

--- app/src/main/java/development/madcat/dagger2example/MainActivity.java
+++ app/src/main/java/development/madcat/dagger2example/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        App.getComponent().injectsMainActivity(this);
+
         networkUtils.test();
         databaseHelper.test();
     }
